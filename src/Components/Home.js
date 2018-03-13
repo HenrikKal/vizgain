8cd7312668d2b7c40b313479d6c254872004efea
HenrikKal <henrik.kalvegren@hotmail.com>
2018-03-13 15:10:13 +0100

    Working on flatlist for exercises

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,15 +8,15 @@ export default class Home extends Component {
   render() {
 
     return (
-    <View>
+    <View style={styles.container}>
 
-    <TouchableOpacity style={styles.button} onPress={() => {Actions.pushw}}>
+    <TouchableOpacity style={styles.button} onPress={Actions.pushw}>
       <Text>Push workout</Text>
     </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={() => {Actions.pullw}}>
+    <TouchableOpacity style={styles.button} onPress={Actions.pullw}>
       <Text>Pull workout</Text>
     </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={() => {Actions.legw}}>
+    <TouchableOpacity style={styles.button} onPress={Actions.legw}>
       <Text>Leg workout</Text>
     </TouchableOpacity>
 
@@ -31,9 +31,20 @@ export default class Home extends Component {
 
 const styles = StyleSheet.create({
 
+  container: {
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
   button: {
-    backgroundColor: 'steelblue',
-    alignItems: 'center'
+    width: 120,
+    height: 40,
+    backgroundColor: 'powderblue',
+    alignItems: 'center',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5
   }
 
 });
--- src/Components/Workout.js
+++ src/Components/Workout.js
@@ -1,18 +1,51 @@
 import React, {Component} from 'react';
-import {View, Text, TextInput, StyleSheet} from 'react-native';
-import ProgressInput from './ProgressInput';
+import {View, Text, TextInput, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
+import {List, ListItem} from 'react-native-elements';
 
 export default class Workout extends Component<props> {
+
+
+  constructor(){
+    super();
+    this.createNewExercise = this.createNewExercise.bind(this);
+
+    this.state = {
+      data: [{'key': 1, 'name': "yo"}]
+    };
+  }
+
+
+
+  createNewExercise(){
+    console.log("new exercise");
+    this.setState({
+      data: data.push({'key: 2, name': "hehe"})
+    });
+
+
+
+  }
+
 
   render() {
     return(
 
-      <View>
+      <View style={styles.container}>
+        <List style={styles.list}>
+          <FlatList
+            data={this.state.data}
+          renderItem={({item}) => (
+            <ListItem title={item.name} />
+          )}/>
+        </List>
         <Text>
           {this.props.name}
+          {'Add an exercise'}
         </Text>
 
-      <ProgressInput/>
+        <TouchableOpacity style={styles.button} onPress={this.createNewExercise}>
+          <Text style={{color:'white'}}>++</Text>
+        </TouchableOpacity>
 
       </View>
 
@@ -21,3 +54,33 @@ export default class Workout extends Component<props> {
     );
   }
 }
+
+
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignSelf: 'center',
+
+  },
+
+  list: {
+    flex: 2,
+
+
+  },
+
+  button: {
+    flex: 1,
+    width: 40,
+    height: 40,
+    backgroundColor: 'powderblue',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+
+  },
+
+});
