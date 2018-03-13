72c647864499f0c03aeb1907754dba494e0d515f
HenrikKal <henrik.kalvegren@hotmail.com>
2018-03-13 15:10:46 +0100

    Added Exercise.js

---
+++ src/Components/Exercise.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import ProgressInput from './ProgressInput';
+
+export default class Exercise extends Component<props> {
+
+  render() {
+    return(
+
+      <View>
+        <Text>
+          {this.props.name}
+        </Text>
+
+      <ProgressInput/>
+
+      </View>
+
+
+
+    );
+  }
+}
